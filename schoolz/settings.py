fd17804175ea84a5ffb51966768961826a5e73fc
Johann Alberts <alberts.johann@gmail.com>
2020-12-31 18:17:34 +0000

    Add gitignore and pull new SECRET_KEY from environment variable

--- schoolz/settings.py
+++ schoolz/settings.py
@@ -11,6 +11,9 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
 from pathlib import Path
+import os
+if os.path.exists('env.py'):
+    import env
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'hf=vfbf%(4r*cw7@7(zfm#^b4_as$43#1m=5(^-fuvx#dtb9!!'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
