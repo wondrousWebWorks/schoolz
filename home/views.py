c8937e4412b77b39b226920996137a928283d669
Johann Alberts <alberts.johann@gmail.com>
2021-01-01 10:04:37 +0000

    Create home template, modify home view to display it and set up static files

--- home/views.py
+++ home/views.py
@@ -2,5 +2,5 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def home(request):
-    return HttpResponse('Hello, world!')
+    return render(request, 'home/index.html')
 
